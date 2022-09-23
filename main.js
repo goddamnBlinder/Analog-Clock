79afc5a6ac77e2b8eb20e3c5476378dc96953b8b
James Chinweike <chinweikechukwudi@gmail.com>
2022-09-24 00:01:37 +0100

    commit

---
+++ main.js
@@ -0,0 +1,15 @@
+let seconds = 0;
+var minutes = 0;
+var hours = 0;
+var d = new Date();
+
+setInterval(
+    function(){
+        d = new Date();
+        seconds = d.getSeconds();
+        minutes = d.getMinutes();
+        hours = d.getHours();
+        document.getElementById("second-hand")
+
+    }
+)
